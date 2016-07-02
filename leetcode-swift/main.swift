4b4e0bebd85fde8b1983c234868c865ad866cb04
Tianyu Wang <wty21cn@gmail.com>
2016-07-02 23:58:19 +0800

    Initial Commit

---
+++ leetcode-swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  leetcode-swift
+//
+//  Created by Rain on 16/7/2.
+//  Copyright © 2016年 Rain Wang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
