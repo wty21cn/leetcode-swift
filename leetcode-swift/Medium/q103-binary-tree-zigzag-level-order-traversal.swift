1b1d573a3ed54122acedcabc47aa76491c494c68
Tianyu Wang <wty21cn@gmail.com>
2016-07-03 19:10:33 +0800

    Add solution2 for q103-binary-tree-zigzag-level-order-traversal

--- leetcode-swift/Medium/q103-binary-tree-zigzag-level-order-traversal.swift
+++ leetcode-swift/Medium/q103-binary-tree-zigzag-level-order-traversal.swift
@@ -2,7 +2,7 @@
 //  q103-binary-tree-zigzag-level-order-traversal.swift
 //  leetcode-swift
 //  Source*   :  https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
-//  Category* :  BinaryTree BFS
+//  Category* :  BinaryTree BFS Stack
 //
 //  Created by Tianyu Wang on 16/7/1.
 //  Github    :  http://github.com/wty21cn
@@ -70,11 +70,70 @@ struct q103 {
         }
     }
     
+    
+    /*
+     
+     Solution 2
+    
+     Use two stack to store each level's nodes, and take advancetage of stack's FILO feature.
+     
+        1.When this level traverse from left to right
+            Add its left child before its right child, then when treaverse the next level, the right child will pop up first
+        2.When this level traverse from right to left
+            Add its right child before its left child, then when treaverse the next level, the left child will pop up first
+    
+                1
+               --->
+              /    \
+             2      3
+            <---------
+           /  \    /  \
+          4    5  6    7
+          -------------->
+     */
+    
+    
+    class Solution2 {
+        func zigzagLevelOrder(root: TreeNode?) -> [[Int]] {
+            
+            var traversal: [[Int]] = []
+            if let root = root {
+                var stack: [[TreeNode]] = [[root],[]]
+                var level = 0
+                while !stack[level % 2].isEmpty {
+                    let nextLevel = (level + 1) % 2
+                    
+                    let node = stack[level % 2].popLast()
+                    if let node = node {
+                        
+                        if level >= traversal.endIndex { traversal.append([]) }
+                        traversal[level].append(node.val)
+                        
+                        switch nextLevel {
+                        case 0:     //right to left
+                            if let r = node.right { stack[nextLevel].append(r) }
+                            if let l = node.left { stack[nextLevel].append(l) }
+                        default:    //left to right
+                            if let l = node.left { stack[nextLevel].append(l) }
+                            if let r = node.right { stack[nextLevel].append(r) }
+                            
+                        }
+                    }
+                    if stack[level % 2].isEmpty {
+                        level += 1
+                    }
+                }
+            }
+            return traversal
+        }
+    }
+    
     static func getSolution() -> Void {
         let root = BinaryTreeBuilder.buildTreeWithNodes([3,9,20,nil,nil,15,7])
         BinaryTreePrinter.print(root!)
         
         
         print(Solution().zigzagLevelOrder(root))
+        print(Solution2().zigzagLevelOrder(root))
     }
 }
