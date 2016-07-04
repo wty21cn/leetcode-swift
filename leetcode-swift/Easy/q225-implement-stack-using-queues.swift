514153a96f8181a14b03fa72dbbaa89c85b5647b
Tianyu Wang <wty21cn@gmail.com>
2016-07-04 20:16:12 +0800

    Solve q225-implement-stack-using-queues

---
+++ leetcode-swift/Easy/q225-implement-stack-using-queues.swift
@@ -0,0 +1,95 @@
+//
+//  q225-implement-stack-using-queues.swift
+//  leetcode-swift
+//  Source*   :  https://leetcode.com/problems/implement-stack-using-queues/
+//  Category* :  Stack Queue
+//
+//  Created by Tianyu Wang on 16/7/4.
+//  Github    :  http://github.com/wty21cn
+//  Website   :  http://wty.im
+//  Linkedin  :  https://www.linkedin.com/in/wty21cn
+//  Mail      :  mailto:wty21cn@gmail.com
+
+
+/**********************************************************************************
+ *
+ * Implement the following operations of a stack using queues.
+ *
+ * push(x) -- Push element x onto stack.
+ *
+ * pop() -- Removes the element on top of the stack.
+ *
+ * top() -- Get the top element.
+ *
+ * empty() -- Return whether the stack is empty.
+ *
+ * Notes:
+ *
+ * You must use only standard operations of a queue -- which means only push to back,
+ * peek/pop from front, size, and is empty operations are valid.
+ *
+ * Depending on your language, queue may not be supported natively. You may simulate
+ * a queue by using a list or deque (double-ended queue), as long as you use only
+ * standard operations of a queue.
+ *
+ * You may assume that all operations are valid (for example, no pop or top operations
+ * will be called on an empty stack).
+ *
+ * Update (2015-06-11):
+ * The class name of the Java function had been updated to MyStack instead of Stack.
+ *
+ * Credits:Special thanks to @jianchao.li.fighter for adding this problem and all test cases.
+ *
+ **********************************************************************************/
+
+
+import Foundation
+
+struct q225 {
+    
+    class Stack {
+        
+        var queue = [Int]()
+        var tmpQueue = [Int]()
+        
+        func push(n: Int) -> Void {
+            queue.append(n)
+        }
+        
+        func pop() -> Void {
+            
+            while queue.count > 0 {
+                tmpQueue.append(queue.removeFirst())
+            }
+            
+            queue.removeFirst()
+            
+            while tmpQueue.count >= 0 {
+                queue.append(queue.removeFirst())
+            }
+        }
+        
+        func top() -> Int {
+            
+            while queue.count > 0 {
+                tmpQueue.append(queue.removeFirst())
+            }
+            
+            let n = queue.removeFirst()
+            tmpQueue.append(n)
+            
+            while tmpQueue.count >= 0 {
+                queue.append(tmpQueue.removeFirst())
+            }
+            return n
+        }
+        
+        func isEmpty() -> Bool {
+            return queue.isEmpty
+        }
+        
+    }
+    
+    static func getSolution() -> Void {
+    }
+}
