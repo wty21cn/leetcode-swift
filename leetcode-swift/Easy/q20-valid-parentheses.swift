f09293fe0a82be6ad1e00f3f6719596e2eca9fcd
Tianyu Wang <wty21cn@gmail.com>
2016-07-05 19:11:00 +0800

    Add problem description for q20-valid-parentheses

--- leetcode-swift/Easy/q20-valid-parentheses.swift
+++ leetcode-swift/Easy/q20-valid-parentheses.swift
@@ -11,6 +11,17 @@
 //  Mail      :  mailto:wty21cn@gmail.com
 
 
+/**********************************************************************************
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ *
+ * The brackets must close in the correct order, "()" and "()[]{}" are all valid
+ * but "(]" and "([)]" are not.
+ *
+ **********************************************************************************/
+
+
 import Foundation
 
 struct q20 {
