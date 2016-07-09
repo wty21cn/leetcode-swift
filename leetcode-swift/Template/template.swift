636e3f403b93ec6426a5c80e03cd33803e65699a
Tianyu Wang <wty21cn@gmail.com>
2016-07-09 16:07:05 +0800

    Update template

--- leetcode-swift/Template/template.swift
+++ leetcode-swift/Template/template.swift
@@ -4,7 +4,7 @@
 //  Source*   :
 //  Category* :  
 //
-//  Created by Tianyu Wang on 16/7/8.
+//  Created by Tianyu Wang on 16/7/9.
 //  Github    :  http://github.com/wty21cn
 //  Website   :  http://wty.im
 //  Linkedin  :  https://www.linkedin.com/in/wty21cn
